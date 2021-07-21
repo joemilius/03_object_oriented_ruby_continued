9408c4e914153e20638bfde95d2e439d0ec638af
JoeMilius <joemilius@gmail.com>
2021-07-21 11:13:14 -0600

    commit tree 60c5ec75e2b12efdc230227f803710666ea6ae96

--- lib/object_oriented_ruby_continued/computer.rb
+++ lib/object_oriented_ruby_continued/computer.rb
@@ -1,4 +1,41 @@
 class Computer
+  attr_accessor :brand, :screen_size, :model_name, :model_year
+  attr_reader :asleep, :last_backed_up_at
+
+  @@all = []
+
+  def initialize (brand:, screen_size:, model_name:, model_year:)
+    @brand = brand
+    @screen_size = screen_size
+    @model_name = model_name
+    @model_year = model_year
+  end
+
+  # def initialize (attributes)
+  #   attributes.each do |attribute_name, value|
+  #     self.send("#{attribute_name}=", value)
+  #   end
+  # end
+  
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.create(attributes)
+    computer = Computer.new(attributes)
+    computer.save
+    computer
+  end
+
+  def self.backed_up
+    all.filter do |computer|
+      computer.last_backed_up_at
+    end
+  end
 
   def sleep
     @asleep = true
@@ -27,20 +64,20 @@ end
 
 # create and return the first computer within this method
 def first_computer
-  computer = Computer.new
-  computer.brand = "Apple"
-  computer.screen_size = 15.0
-  computer.model_name = "Macbook Pro"
-  computer.model_year = 2012
-  computer
+  first_computer = Computer.new(brand: "Apple", screen_size: 15.0, model_name: "MacBook Pro", model_year: 2012)
+  # computer.brand = "Apple"
+  # computer.screen_size = 15.0
+  # computer.model_name = "Macbook Pro"
+  # computer.model_year = 2012
+  # computer
 end
 
 # create and return the second computer within this method
 def second_computer
-  computer = Computer.new
-  computer.brand = "Apple"
-  computer.screen_size = 13.0
-  computer.model_name = "Macbook Air"
-  computer.model_year = 2015
-  computer
+  second_computer = Computer.new(
+  brand: "Apple",
+  screen_size: 13.0,
+  model_name: "Macbook Air",
+  model_year: 2015
+  )
 end
